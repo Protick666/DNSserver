6f52c46e477bf7f9cb13408f7520e43bf5b039e4
protickbhowmick <protickbhowmick@priyo.com>
2022-09-19 00:29:54 -0400

    init

--- ttl_resolver.py
+++ ttl_resolver.py
@@ -27,12 +27,30 @@ except ImportError:
     print("Missing dependency dnslib: <https://pypi.python.org/pypi/dnslib>. Please install it with `pip`.")
     sys.exit(2)
 
+'''
+Worries:
 
+Is this logging best ??
+
+Alternatives ??
+
+Identifier: 1 -> NXDOMAIN, 2 -> SERVFAIL, 3 - EMPTY RESPONSE
+
+'''
+
+'''
+Checking:
+    see telemtert nx domain response
+    log directory
+    resolv file
+    transfer check
+    lum transfer check
+'''
 
 logger = logging.getLogger('my_logger')
 logger.setLevel(logging.INFO)
 # TODO create log directory
-handler = RotatingFileHandler('log/my_log.log', maxBytes=100000000, backupCount=1000)
+handler = RotatingFileHandler('log/my_log.log', maxBytes=50000000, backupCount=1000)
 logger.addHandler(handler)
 
 
@@ -41,64 +59,45 @@ class DomainName(str):
         return DomainName(item + '.' + self)
 
 
-D = DomainName('securekey.app.')
-
-'''
-666 resolver_ip timestamp served_ip query query_type
-'''
-
-##
-
-#########
-
-
-
-soa_record = SOA(
-    mname=D.ns1,  # primary name server
-    rname=D.hostmaster,  # email of the domain administrator
-    times=(
-        201307231,  # serial number
-        60 * 60 * 1,  # refresh
-        60 * 60 * 3,  # retry
-        60 * 60 * 24,  # expire
-        60 * 60 * 1,  # minimum
-    )
-)
-
-ns_records = [NS(D.ns1), NS(D.ns2)]
-
-# ns_records = [NS(D.ns1), NS(D.ns2)]
-# records = {
-#     D: [A(IP), AAAA((0,) * 16), MX(D.mail), soa_record] + ns_records,
-#     D.ns1: [A(IP)],  # MX and NS records must never point to a CNAME alias (RFC 2181 section 10.3)
-#     D.ns2: [A(IP)],
-#     D.mail: [A(IP)],
-#     D.andrei: [CNAME(D)],
-# }
-
-# records = {
-#     D: [A(IP), soa_record] + ns_records,
-#     D.ns1: [A(NS_IP)],  # MX and NS records must never point to a CNAME alias (RFC 2181 section 10.3)
-#     D.ns2: [A(NS_IP)],
-#     D.__getattr__("*"): [A(IP)],
-# }
-
-
 record_dict = {}
-
 index_to_record_tuple = {}
-
-for ip in get_all_ips():
-    record_tuple = {
-        D: [A(ip), soa_record] + ns_records,
-        D.ns1: [A(NS_IP)],  # MX and NS records must never point to a CNAME alias (RFC 2181 section 10.3)
-        D.ns2: [A(NS_IP)],
-        D.__getattr__("*"): [A(ip)],
-    }
-    record_dict[ip] = record_tuple
+base_domain_name = None
+D = None
+soa_record = None
+ns_records = None
 
 
-# NX domain
+def preprocess_info(domain_name):
+    # base_domain_name = 'securekey.app.'
+    global base_domain_name, D, soa_record, ns_records, record_dict
+
+    base_domain_name = domain_name
+    D = DomainName(base_domain_name)
+
+    soa_record = SOA(
+        mname=D.ns1,  # primary name server
+        rname=D.hostmaster,  # email of the domain administrator
+        times=(
+            201307231,  # serial number
+            60 * 60 * 1,  # refresh
+            60 * 60 * 3,  # retry
+            60 * 60 * 24,  # expire
+            60 * 60 * 1,  # minimum
+        )
+    )
+
+    ns_records = [NS(D.ns1), NS(D.ns2)]
+
+    for ip in get_all_ips():
+        record_tuple = {
+            D: [A(ip), soa_record] + ns_records,
+            D.ns1: [A(NS_IP)],  # MX and NS records must never point to a CNAME alias (RFC 2181 section 10.3)
+            D.ns2: [A(NS_IP)],
+            D.__getattr__("*"): [A(ip)],
+        }
+        record_dict[ip] = record_tuple
+
+
 def dns_response(data, client_ip):
     # TODO see sudhu A record i ashe kina**
     # TODO exp id er presense dekhio: zeus_reload
@@ -114,9 +113,8 @@ def dns_response(data, client_ip):
 
     logger.info("Query from {} {} {}".format(client_ip, qn, qt))
 
-    # TODO here
-    if qt == 'A' and (qn == 'ns1.securekey.app.' or qn == 'ns2.securekey.app.'):
-        reply.add_answer(RR(rname=qname, rtype=getattr(QTYPE, 'A'), rclass=1, ttl=1800, rdata=A(NS_IP)))
+    if qt == 'A' and (qn == 'ns1.{}'.format(base_domain_name) or qn == 'ns2.{}'.format(base_domain_name)):
+        reply.add_answer(RR(rname=qname, rtype=getattr(QTYPE, 'A'), rclass=1, ttl=3600, rdata=A(NS_IP)))
         return reply.pack()
     elif qt != 'A':
         return reply.pack()
@@ -126,35 +124,44 @@ def dns_response(data, client_ip):
         logger.info("nxmal {} {} {} {}".format(client_ip, time.time(), qn, qt))
         reply.header.rcode = 3
         return reply.pack()
+
     if 'event-' in qn:
-        # TODO return NXdomain
-        #logger.info("nxmal {} {} {}".format(client_ip, time.time(), qn))
         logger.info("goodevent {} {} {} {} {}".format(client_ip, time.time(), "xxx", qn, qt))
         reply.header.rcode = 3
         return reply.pack()
 
-    # ${uuid_str}.${exp_id}.${TTL}.${domain.asn}.${bucket_number}.${URL}
-
+    # TODO changes: new identifier
+    # ${uuid_str}.${exp_id}.${TTL}.${domain.asn}.${bucket_number}.{$exp_mid_behaviour}.${URL}
     meta_info_list = qn.split(".")
-    uuid, exp_id, ttl, asn, bucket = meta_info_list[0], meta_info_list[1], meta_info_list[2], meta_info_list[3], meta_info_list[4]
+    uuid, exp_id, ttl, asn, bucket, exp_mid_behaviour = meta_info_list[0], \
+                                                        meta_info_list[1], meta_info_list[2], \
+                                                        meta_info_list[3], meta_info_list[4], \
+                                                        meta_info_list[5]
     ttl = int(ttl) * 60
     # 1, 2, 3
     mode = get_mode(exp_id=exp_id)
 
     if mode == 1:
         if is_lum_ip(resolver_ip=client_ip):
-            #logger.info("lum ip")
             chosen_ip = lum_resolver_list[0]
         else:
             chosen_ip = get_ip_wrapper(resolver_ip=client_ip, uuid=uuid, ttl=ttl, redis_lock=redis_lock, logger=logger)
     elif mode == 3:
         chosen_ip = phase_2_ip_list[0]
     else:
-        # returning NXdomain
-        logger.info("nxmode {} {} {} {}".format(client_ip, time.time(), qn, qt))
-        reply.header.rcode = 3
-        return reply.pack()
+        # mode  = 2
+        # Identifier: 1 -> NXDOMAIN, 2 -> SERVFAIL, 3 - EMPTY RESPONSE
+        # TODO changes log format
+        exp_mid_behaviour = int(exp_mid_behaviour)
+        logger.info("replymid {} {} {} {} {}".format(exp_mid_behaviour, client_ip, time.time(), qn, qt))
 
+        if exp_mid_behaviour == 1:
+            reply.header.rcode = 3
+        elif exp_mid_behaviour == 2:
+            reply.header.rcode = 2
+        elif exp_mid_behaviour == 3:
+            return reply.pack()
+        return reply.pack()
 
     chosen_record = record_dict[chosen_ip]
 
@@ -164,23 +171,11 @@ def dns_response(data, client_ip):
                 for rdata in rrs:
                     rqt = rdata.__class__.__name__
                     if qt in ['*', rqt]:
-                        reply.add_answer(RR(rname=qname, rtype=getattr(QTYPE, rqt), rclass=1, ttl=int(ttl), rdata=rdata))
+                        reply.add_answer(
+                            RR(rname=qname, rtype=getattr(QTYPE, rqt), rclass=1, ttl=int(ttl), rdata=rdata))
 
-        # for rdata in ns_records:
-        #     reply.add_ar(RR(rname=D, rtype=QTYPE.NS, rclass=1, ttl=int(ttl), rdata=rdata))
         reply.add_auth(RR(rname=D, rtype=QTYPE.SOA, rclass=1, ttl=int(ttl), rdata=soa_record))
     logger.info("good {} {} {} {} {}".format(client_ip, time.time(), chosen_ip, qn, qt))
-
-    # print("---- Reply:\n", reply)
-
-    '''
-
-    666 resolver_ip timestamp served_ip query query_type
-    
-    print("\n\n%s request %s (%s %s):" % (self.__class__.__name__[:3], now, self.client_address[0],
-                                              self.client_address[1]))
-
-    '''
 
     return reply.pack()
 
@@ -194,14 +189,11 @@ class BaseRequestHandler(socketserver.BaseRequestHandler):
         raise NotImplementedError
 
     def handle(self):
-        now = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')
         c_ip = self.client_address[0]
-        # print("hit")
-        # print("\n\n%s request %s (%s %s):" % (self.__class__.__name__[:3], now, self.client_address[0],
-        #                                       self.client_address[1]))
+
         try:
             data = self.get_data()
-            #print(len(data), data)  # repr(data).replace('\\x', '')[1:-1]
+            # print(len(data), data)  # repr(data).replace('\\x', '')[1:-1]
             self.send_data(dns_response(data=data, client_ip=c_ip))
         except Exception:
             traceback.print_exc(file=sys.stderr)
@@ -231,16 +223,24 @@ class UDPRequestHandler(BaseRequestHandler):
     def send_data(self, data):
         return self.request[1].sendto(data, self.client_address)
 
-# CHECK TCP
+
 def main():
     parser = argparse.ArgumentParser(description='Start a DNS implemented in Python.')
     parser = argparse.ArgumentParser(description='Start a DNS implemented in Python. Usually DNSs use UDP on port 53.')
     parser.add_argument('--port', default=5053, type=int, help='The port to listen on.')
+    parser.add_argument('--base_domain', type=str, help='The base domain')
     parser.add_argument('--tcp', action='store_true', help='Listen to TCP connections.')
     parser.add_argument('--udp', action='store_true', help='Listen to UDP datagrams.')
 
     args = parser.parse_args()
-    if not (args.udp or args.tcp): parser.error("Please select at least one of --udp or --tcp.")
+
+    if not args.base_domain:
+        parser.error("Please provide the base domain name with ")
+    domain = args.base_domain
+    preprocess_info(domain_name=domain)
+
+    if not (args.udp or args.tcp):
+        parser.error("Please select at least one of --udp or --tcp.")
 
     print("Starting nameserver...")
 
