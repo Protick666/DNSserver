8ef2227be8b6ccceccc99097abef6e88eb9eb9ea
protickbhowmick <protickbhowmick@priyo.com>
2022-10-21 01:16:30 -0400

    init

--- dnssec_resolver.py
+++ dnssec_resolver.py
@@ -132,9 +132,9 @@ def dns_response(data, client_ip, is_udp):
     re_msg = DNSRecord.parse(answer.to_wire())
     tc_bit = re_msg.header.tc
 
-    logger.info("good {} {} {} {} {} {} {} {} tc: {} do: {} {} {}".format(query_format, client_ip,
+    logger.info("good {} {} {} {} {} {} {} {} tc: {} do: {} {}".format(query_format, client_ip,
                                                       time.time(), m_ode, c_ip, chosen_container_ip, qn,
-                                                                             qt, tc_bit, ednsflag, cline_Str, str(re_msg)))
+                                                                             qt, tc_bit, ednsflag, cline_Str))
 
     return response_as_byte_arr
 
